4e816cd30077d7b13edacc1c3ca62fc9e4e2f632
Anton <kondratov247@mail.ru>
2021-06-03 11:52:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,17 @@ def write_msg(id, text):
 for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
              if event.to_me:
+                print(event.attachments)
+                print()
+                print(event.raw)
                 if event.text == "Привет":
                   write_msg(event.user_id, "и тебе привет")
                 elif event.text == "как дела?":
                     write_msg(event.user_id, "норм")
+                elif not(event.text) and event.attachments['attach1_type'] == "sticker":
+                  write_msg(event.user_id, "Списибо за стикер")
+                  vk_session.method("messages.sendSticker", {'peer_id' : event.user_id, "sticker_id":14432, "random_id": 0})
+                elif 'emoji' in event.raw[6]:
+                   write_msg(event.user_id, '🤗')
+                elif event.text:
+                  write_msg(event.user_id, "Я тебя не понял")
